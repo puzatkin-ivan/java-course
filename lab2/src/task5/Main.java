94708957af00cacafc3ee4a69dd0dc949f430c0e
puzatkin-ivan <puzatkin1998@gmail.com>
2018-11-05 17:23:20 +0300

    Сделал 5 задание по лабораторной работе

---
+++ lab2/src/task5/Main.java
@@ -0,0 +1,65 @@
+package task5;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    try
+    {
+      if (args.length != 2) {
+        System.out.println("Incorrect number of arguments");
+        return;
+      }
+
+      FileReader iStream = new FileReader(args[0]);
+
+      ArrayList<String> wordsList = new ArrayList<>();
+      Map<String, Integer> storage = processFile(iStream, wordsList);
+      printWords(storage, wordsList, Integer.valueOf(args[1]));
+    }
+    catch (Exception ex)
+    {
+      System.out.println(ex.toString());
+    }
+  }
+
+  private static Map<String, Integer> processFile(FileReader iStream, ArrayList<String> priorityList) throws IOException {
+    BufferedReader reader = new BufferedReader(iStream);
+    Map<String, Integer> storage = new HashMap<>();
+
+    String line = reader.readLine();
+    while (line != null) {
+      String[] lineArr = line.split(" ");
+      processLine(lineArr, storage, priorityList);
+      line = reader.readLine();
+    }
+
+    return storage;
+  }
+
+  private static void processLine(String[] line, Map<String, Integer> storage, ArrayList<String> priorityList)
+  {
+    for (String item : line) {
+      if (storage.containsKey(item)) {
+        storage.put(item, storage.get(item) + 1);
+        int index = priorityList.indexOf(item);
+        if (index != (priorityList.size() - 1)) {
+          priorityList.remove(item);
+          priorityList.add(index + 1, item);
+        }
+      } else {
+        storage.put(item, 1);
+        priorityList.add(0, item);
+      }
+    }
+  }
+
+  private static void printWords(Map<String, Integer> orderedMap, ArrayList<String> wordsList, int countWords) {
+    int count = 0;
+    for (int j = (wordsList.size() - 1); j >= 0 && count != countWords; j--) {
+      String temp = wordsList.get(j);
+      System.out.println(temp + " " + orderedMap.get(temp));
+      ++count;
+    }
+  }
+}
